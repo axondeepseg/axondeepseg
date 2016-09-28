da6062723dbe56cb65ee33dc2ed28bb01f5949a2
[vherman3] <victor.herman3@gmail.com>
2016-09-28 09:47:19 -0400

    some improvements of the guideline

--- guideline.py
+++ guideline.py
@@ -6,12 +6,13 @@
 # input data to build the training set
 path_data = './victor/data'
 
-# output path of training data
+# output path of training data path
 path_training = './trainingset'
 
 # output path for trained U-Net
 path_model = './models/Unet_new'
-path_model_init = './models/Unet_parameters1'
+# optional input path of a model to initialize the training
+path_model_init = './models/Unet_init'
 
 # input to train the mrf. Here we don't use all data for the MRF for faster results and redundancy.
 path_mrf_training = ['./victor/data/data2', './victor/data/data4']
@@ -24,7 +25,7 @@ path_my_data = './victor/my_data/data4'
 from AxonDeepSeg.learning.data_construction import build_data
 build_data(path_data, path_training, trainRatio=0.80)
 
-# OPTION 1: Train on CPU
+# OPTION 1: Local Train
 # Training the U-Net from a path_training
 from AxonDeepSeg.learn_model import learn_model
 learn_model(path_training, path_model, learning_rate=0.005)
@@ -33,17 +34,18 @@ learn_model(path_training, path_model,path_model_init, learning_rate=0.002)
 
 # OPTION 2: Train on GPU
 # copy training data + model (in case you start from an existing model) onto neuropoly@ssh
-scp -r AxonDeepSeg neuropoly@bireli.neuro.polymtl.ca
+scp -r AxonDeepSeg neuropoly@bireli.neuro.polymtl.ca:
 scp -r path_training neuropoly@bireli.neuro.polymtl.ca:my_project #path on bireli : path_bireli_training
-scp -r path_model_init neuropoly@bireli.neuro.polymtl.ca:my_project # path on bireli : path_bireli_model_init
+scp -r path_model_init neuropoly@bireli.neuro.polymtl.ca:my_project/models # path on bireli : path_bireli_model_init
 # Connect to bireli using ssh neuropoly@ssh
-cd AxonDeepSeg
+git clone https://github.com/neuropoly/axondeepseg.git
+cd axondeepseg/AxonDeepSeg
 # sub-option1: if you don't have an initial model:
 python learn_model.py -p path_bireli_training -m path_bireli_model -lr 0.0005 # result : path_bireli_model
 # sub-option2: if you do have an initial model:
 python learn_model.py -p path_bireli_training -m path_bireli_model -m_init path_bireli_model_init  -lr 0.0005
 # In a local Terminal window, visualize to visualize the training performances:
-scp -r path_bireli_model path_model
+scp -r neuropoly@bireli.neuro.polymtl.ca:my_project/models/model path_model
 
 #Visualization of the training
 from AxonDeepSeg.evaluation.visualization import visualize_learning
@@ -58,16 +60,30 @@ learn_mrf(path_mrf_training, path_model, path_mrf)
 
 #----------------------Axon segmentation with a trained model and trained mrf---------------------#
 from AxonDeepSeg.apply_model import axon_segmentation
+# Ground Truth need to be in path_my_data
 axon_segmentation(path_my_data, path_model, path_mrf)
+# Outputs :
+# Axon segmentation masks AxonMask.mat and AxonSeg.jpeg
+# Segmentation array after and before mrf in results.pkl
 
-#----------------------Myelin segmentation from Axon segmentation--------------------#
+#----------------------Myelin segmentation from axon segmentation--------------------#
 from AxonDeepSeg.apply_model import myelin
+# Axon_segmentation() has to be already runned
 myelin(path_my_data)
+# Outputs in (path_my_data) :
+# Myelin segmentation mask, myelin.jpg
+# Metrics evalution of the segmentation, axonlist.mat
 
 #----------------------Axon and Myelin segmentation--------------------#
 from AxonDeepSeg.apply_model import pipeline
 pipeline(path_my_data, path_model, path_mrf)
+# Outputs :
+# Outputs from myelin() and axon_segmentation()
 
 #----------------------Visualization of the results--------------------#
 from AxonDeepSeg.evaluation.visualization import visualize_results
 visualize_results(path_my_data)
+# Outputs :
+# Plots of the axon segmentation and the myelin segmentation
+# if my_data does have a groundtruth : report_results.txt in path_my_data
+
--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['matplotlib', 'numpy', 'scipy', 'scikit-image', 'scikit-learn', 'tabulate'],
+    install_requires=['matplotlib', 'numpy', 'scipy', 'scikit-image', 'scikit-learn', 'tabulate', 'Pillow'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
