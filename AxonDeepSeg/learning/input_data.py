261acd913ab98dcd009619dd87cadf343a47d6d6
vherman3 <victor.herman3@gmail.com>
2017-01-10 10:43:37 -0500

    solve issue on data augmentation

--- AxonDeepSeg/learning/input_data.py
+++ AxonDeepSeg/learning/input_data.py
@@ -153,7 +153,7 @@ def rescaling(patch):
             image_rescale, mask_rescale = patches[i]
 
         mask_rescale = preprocessing.binarize(np.array(mask_rescale), threshold=0.5)
-        rescaled_patch = [image_rescale, mask_rescale.astype(int)]
+        rescaled_patch = [image_rescale.astype(np.uint8), mask_rescale.astype(np.uint8)]
 
     return rescaled_patch
 
@@ -166,12 +166,11 @@ def random_rotation(patch):
     mask = patch[1]
 
     angle = np.random.uniform(5, 89, 1)
-
     image_rotated = transform.rotate(img, angle, resize = False, mode = 'symmetric',preserve_range=True)
     gt_rotated = transform.rotate(mask, angle, resize = False, mode = 'symmetric', preserve_range=True)
-    gt_rotated = (preprocessing.binarize(gt_rotated, threshold=0.5)).astype(int)
+    gt_rotated = (preprocessing.binarize(gt_rotated, threshold=0.5))
 
-    return [image_rotated, gt_rotated]
+    return [image_rotated.astype(np.uint8), gt_rotated.astype(np.uint8)]
 
 
 def random_transformation(patch):
@@ -238,13 +237,15 @@ class input_data:
             image = imread(self.path + 'image_%s.jpeg' % indice, flatten=False, mode='L')
             mask = preprocessing.binarize(imread(self.path + 'mask_%s.jpeg' % indice, flatten=False, mode='L'), threshold=125)
 
+            if augmented_data:
+                [image, mask] = random_transformation([image, mask])            
+ 
             #-----PreProcessing --------
             image = exposure.equalize_hist(image) #histogram equalization
             image = (image - np.mean(image))/np.std(image) #data whitening
-            #---------------------------
 
-            if augmented_data:
-                [image, mask] = random_transformation([image, mask])
+
+            #---------------------------
 
             batch_x.append(image)
             if i == 0:
