Fixed the balanced weights formula by normalizing them with the total balanced weights mean

AxonDeepSeg/data/input_data.py
@@ -342,7 +342,9 @@ class input_data:
             for class_ in range(n):
                 weights_multiplier = 1
                 if weights_modifier['balanced_activate'] == True:
-                    weights_multiplier = np.multiply(weights_multiplier, weights_modifier['balanced_weights'][class_])
+                    balanced_weights = weights_modifier['balanced_weights'][class_]
+                    balanced_weights = balanced_weights/np.mean(balanced_weights)
+                    weights_multiplier = np.multiply(weights_multiplier, balanced_weights)
                 if weights_modifier['boundaries_activate'] == True:
                     weights_multiplier = np.multiply(weights_multiplier, weights_intermediate[:,:,class_])
                 real_weights += np.multiply(real_mask[:,:,class_],weights_multiplier)