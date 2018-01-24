f8505397f60e818cdb4536113cf05165b79fa1e7
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2018-01-24 17:58:00 -0500

    Revert "modifs for package path"
    This reverts commit 220a31d769a157e74b2342bfdb933f5d98f7d334.

--- AxonDeepSeg/integrity_test.py
+++ AxonDeepSeg/integrity_test.py
@@ -4,62 +4,54 @@
 # Basic integrity test to check is AxonDeepSeg is correctly installed
 # Launches a segmentation in the data_test folder
 
-def integrity_test():
+try:
 
-    try:
+    import json
+    import os
+    from AxonDeepSeg.testing.segmentation_scoring import *
+    from time import time
 
-        import json
-        import os
-        from AxonDeepSeg.testing.segmentation_scoring import *
-        from time import time
-        from AxonDeepSeg.apply_model import axon_segmentation
-        from scipy.misc import imread, imsave
+    from scipy.misc import imread, imsave
 
-        # input parameters
+    # input parameters
 
-        path_testing = "../AxonDeepSeg/data_test/"
-        model_name = 'default_SEM_model_v1'
-        path_model = '../AxonDeepSeg/models/' + model_name
+    path_testing = "../AxonDeepSeg/data_test/"
+    model_name = 'default_SEM_model_v1'
+    path_model = '../AxonDeepSeg/models/' + model_name
 
 
-        #path_testing = "../AxonDeepSeg/data_test/"
-        #model_name = 'default_SEM_model_v1'
-        #path_model = '../AxonDeepSeg/models/' + model_name
+    path_configfile = path_model + '/config_network.json'
+
+    if not os.path.exists(path_model):
+        os.makedirs(path_model)
+
+    with open(path_configfile, 'r') as fd:
+        config_network = json.loads(fd.read())
 
 
-        path_configfile = path_model + '/config_network.json'
+    from AxonDeepSeg.apply_model import axon_segmentation
 
-        if not os.path.exists(path_model):
-            os.makedirs(path_model)
-
-        with open(path_configfile, 'r') as fd:
-            config_network = json.loads(fd.read())
-
-        prediction = axon_segmentation([path_testing], ["image.png"], path_model, config_network,verbosity_level=0)
+    prediction = axon_segmentation([path_testing], ["image.png"], path_model, config_network,verbosity_level=0)
 
 
-        mask = imread(path_testing + '/mask.png', flatten=True)
-        pred = imread(path_testing + '/AxonDeepSeg.png', flatten=True)
+    mask = imread(path_testing + '/mask.png', flatten=True)
+    pred = imread(path_testing + '/AxonDeepSeg.png', flatten=True)
 
 
-        gt_axon = mask > 200
-        gt_myelin = np.logical_and(mask >= 50, mask <= 200)
+    gt_axon = mask > 200
+    gt_myelin = np.logical_and(mask >= 50, mask <= 200)
 
-        pred_axon = pred > 200
-        pred_myelin = np.logical_and(pred >= 50, pred <= 200)
+    pred_axon = pred > 200
+    pred_myelin = np.logical_and(pred >= 50, pred <= 200)
 
-        dice_axon = pw_dice(pred_axon, gt_axon)
-        dice_myelin = pw_dice(pred_myelin, gt_myelin)
+    dice_axon = pw_dice(pred_axon, gt_axon)
+    dice_myelin = pw_dice(pred_myelin, gt_myelin)
 
-        print_message = "* * * Integrity test passed. AxonDeepSeg is correctly installed. * * * "
+    print "* * * Integrity test passed. AxonDeepSeg is correctly installed. * * * "
 
-    except IOError:
+except IOError:
 
-        print_message = "Integrity test failed... "
-
-    return print_message
-
-
+    print "Integrity test failed... "
 
 
 
