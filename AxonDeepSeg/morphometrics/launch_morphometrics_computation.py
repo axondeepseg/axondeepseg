7b5a16d2b96b46dadcd77684aa731e59a7841e08
Mathieu Boudreau <emb6150@gmail.com>
2018-05-09 19:28:07 -0230

    [TEST] Add tests and exception handing for launch_morphometrics_computation

--- AxonDeepSeg/morphometrics/launch_morphometrics_computation.py
+++ AxonDeepSeg/morphometrics/launch_morphometrics_computation.py
@@ -18,28 +18,32 @@ def launch_morphometrics_computation(path_img,path_prediction):
 	:param path_prediction: path of the segmented image (output of AxonDeepSeg)
 	:return: none.
 	'''
-
-	# Read image
-	img = imageio.imread(path_img)
+	try:
+		# Read image
+		img = imageio.imread(path_img)
 	
-	# Read prediction
-	pred = imageio.imread(path_prediction)
+		# Read prediction
+		pred = imageio.imread(path_prediction)
+	except (IOError, OSError) as e:
+		print ("launch_morphometrics_computation: " + e.message)
+		raise
+	else:
 
-	# Get axon and myelin masks
-	pred_axon = pred > 200
-	pred_myelin = np.logical_and(pred >= 50, pred <= 200)
+		# Get axon and myelin masks
+		pred_axon = pred > 200
+		pred_myelin = np.logical_and(pred >= 50, pred <= 200)
 
-	# Get folder path
-	path_folder, file_name = os.path.split(path_img)
+		# Get folder path
+		path_folder, file_name = os.path.split(path_img)
 
-	# Compute and save axon morphometrics
-	stats_array = get_axon_morphometrics(pred_axon,path_folder)
-	save_axon_morphometrics(path_folder,stats_array)
+		# Compute and save axon morphometrics
+		stats_array = get_axon_morphometrics(pred_axon,path_folder)
+		save_axon_morphometrics(path_folder,stats_array)
 
-	# Generate and save displays of axon morphometrics
-	display_axon_diameter(img,path_prediction,pred_axon,pred_myelin)
+		# Generate and save displays of axon morphometrics
+		display_axon_diameter(img,path_prediction,pred_axon,pred_myelin)
 
-	# Compute and save aggregate morphometrics
-	aggregate_metrics = get_aggregate_morphometrics(pred_axon,pred_myelin,path_folder)
-	write_aggregate_morphometrics(path_folder,aggregate_metrics)
+		# Compute and save aggregate morphometrics
+		aggregate_metrics = get_aggregate_morphometrics(pred_axon,pred_myelin,path_folder)
+		write_aggregate_morphometrics(path_folder,aggregate_metrics)
 
---
+++ test/morphometrics/test_launch_morphometrics_computation.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+import pytest
+import os
+import random
+import string
+
+from AxonDeepSeg.morphometrics.launch_morphometrics_computation import launch_morphometrics_computation
+
+
+class TestCore(object):
+    def setup(self):
+        self.fullPath = os.path.dirname(os.path.abspath(__file__))
+        # Move up to the test directory, "test/"
+        self.testPath = os.path.split(self.fullPath)[0]
+        self.dataPath = os.path.join(self.testPath, '__test_files__')
+
+    def teardown(self):
+        pass
+
+    #--------------launch_morphometrics_computation.py tests--------------#
+    def test_launch_morphometrics_computation_saves_files_in_expected_location(self):
+        expectedFiles = {'aggregate_morphometrics.txt',
+                         'AxonDeepSeg_map-axondiameter.png',
+                         'axonlist.npy'
+                         }
+
+        pathImg = os.path.join(self.dataPath, 'image.png')
+        pathPrediction = os.path.join(self.dataPath,
+                                      'AxonDeepSeg_seg-axonmyelin.png')
+
+        launch_morphometrics_computation(pathImg, pathPrediction)
+
+        for fileName in expectedFiles:
+            fullFilePath = os.path.join(self.dataPath, fileName)
+            assert os.path.isfile(fullFilePath)
+            os.remove(fullFilePath)
+
+    def test_launch_morphometrics_computation_throws_error_for_missing_file(self):
+        nonExistingFile = ''.join(random.choice(string.lowercase) for i in range(16))
+        pathPrediction = os.path.join(self.dataPath,
+                                      'AxonDeepSeg_seg-axonmyelin.png')
+
+        with pytest.raises((IOError, OSError)):
+            launch_morphometrics_computation(nonExistingFile, pathPrediction)
