808c882d9803de2164ce07d8d4795cfd6d46faf0
Antoine Moevus <polymtlmaf88@gmail.com>
2019-03-06 16:03:41 -0500

    Remove unused Matplotlib importation

--- AxonDeepSeg/morphometrics/launch_morphometrics_computation.py
+++ AxonDeepSeg/morphometrics/launch_morphometrics_computation.py
@@ -4,47 +4,47 @@ import numpy as np
 import math
 import os
 import imageio
-from sys import platform as _platform
-
+from sys import platform as _platformgi
 from AxonDeepSeg.morphometrics.compute_morphometrics import *
 import AxonDeepSeg.ads_utils
 
 
-def launch_morphometrics_computation(path_img,path_prediction):
-	'''
+def launch_morphometrics_computation(path_img, path_prediction):
+    """
 	This function is equivalent to the morphometrics_extraction notebook of AxonDeepSeg.
 	It automatically performs all steps (computations, savings, displays,...) of the 
 	morphometrics extraction of a given sample.
 	:param path_img: path of the input image (microscopy sample)
 	:param path_prediction: path of the segmented image (output of AxonDeepSeg)
 	:return: none.
-	'''
-	try:
-		# Read image
-		img = imageio.imread(path_img)
-	
-		# Read prediction
-		pred = imageio.imread(path_prediction)
-	except (IOError, OSError) as e:
-		print(("launch_morphometrics_computation: " + str(e)))
-		raise
-	else:
+	"""
+    try:
+        # Read image
+        img = imageio.imread(path_img)
 
-		# Get axon and myelin masks
-		pred_axon = pred > 200
-		pred_myelin = np.logical_and(pred >= 50, pred <= 200)
+        # Read prediction
+        pred = imageio.imread(path_prediction)
+    except (IOError, OSError) as e:
+        print(("launch_morphometrics_computation: " + str(e)))
+        raise
+    else:
 
-		# Get folder path
-		path_folder, file_name = os.path.split(path_img)
+        # Get axon and myelin masks
+        pred_axon = pred > 200
+        pred_myelin = np.logical_and(pred >= 50, pred <= 200)
 
-		# Compute and save axon morphometrics
-		stats_array = get_axon_morphometrics(pred_axon,path_folder)
-		save_axon_morphometrics(path_folder,stats_array)
+        # Get folder path
+        path_folder, file_name = os.path.split(path_img)
 
-		# Generate and save displays of axon morphometrics
-		display_axon_diameter(img,path_prediction,pred_axon,pred_myelin)
+        # Compute and save axon morphometrics
+        stats_array = get_axon_morphometrics(pred_axon, path_folder)
+        save_axon_morphometrics(path_folder, stats_array)
 
-		# Compute and save aggregate morphometrics
-		aggregate_metrics = get_aggregate_morphometrics(pred_axon,pred_myelin,path_folder)
-		write_aggregate_morphometrics(path_folder,aggregate_metrics)
+        # Generate and save displays of axon morphometrics
+        display_axon_diameter(img, path_prediction, pred_axon, pred_myelin)
 
+        # Compute and save aggregate morphometrics
+        aggregate_metrics = get_aggregate_morphometrics(
+            pred_axon, pred_myelin, path_folder
+        )
+        write_aggregate_morphometrics(path_folder, aggregate_metrics)
