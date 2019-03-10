37582f42511c343fc2d77158f1555870451f7ca6
Stoyan I. Asenov <43790661+Stoyan-I-A@users.noreply.github.com>
2019-03-10 17:38:14 -0400

    Issues #162 and #173
    - Implemented the GUI design
    - The user can now select which model to apply by selecting it in the ComboBox

--- ads_plugin.py
+++ ads_plugin.py
@@ -1,4 +1,6 @@
 import wx
+import wx.lib.agw.hyperlink as hl
+
 import fsleyes.controls.controlpanel as ctrlpanel
 import fsleyes.actions.loadoverlay as ovLoad
 
@@ -8,26 +10,57 @@ from PIL import Image
 import os
 import json
 
+import AxonDeepSeg
 from AxonDeepSeg.apply_model import axon_segmentation
 
 class Ads_control_v2(ctrlpanel.ControlPanel):
 
     def __init__(self, *args, **kwargs):
         ctrlpanel.ControlPanel.__init__(self, *args, **kwargs)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        self.SetSizer(sizer)
-        sizer.Add(wx.StaticText(self, label='Ads_plugin_v2_controls'),
-                  flag=wx.EXPAND, proportion=1)
 
-        self.imageDirPath = None
+        # Adding sizers to the control panel
+        sizerH = wx.BoxSizer(wx.HORIZONTAL)
+        sizerV1 = wx.BoxSizer(wx.VERTICAL)
+        sizerV2 = wx.BoxSizer(wx.VERTICAL)
+        sizerV3 = wx.BoxSizer(wx.VERTICAL)
+        sizerV4 = wx.BoxSizer(wx.VERTICAL)
+
+        sizerH.Add(sizerV1, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM, border=10)
+        sizerH.Add(sizerV2, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM, border=10)
+        sizerH.Add(sizerV3, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM, border=10)
+        sizerH.Add(sizerV4, 1, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP | wx.BOTTOM, border=10)
+
+        # Adding widgets to the control panel
+
+        ADS_logo = self.getLogo()
+        sizerV1.Add(ADS_logo, flag=wx.SHAPED, proportion=1)
+
+
+        citationBox = wx.TextCtrl(self, value=self.getCitation(), size=(100, 50), style=wx.TE_MULTILINE)
+        sizerV1.Add(citationBox, flag=wx.SHAPED, proportion=1)
+
 
         loadPng_button = wx.Button(self, label = "Load PNG file")
         loadPng_button.Bind(wx.EVT_BUTTON, self.onLoadPngButton)
-        sizer.Add(loadPng_button, flag=wx.SHAPED, proportion=1)
+        sizerV2.Add(loadPng_button, flag=wx.SHAPED, proportion=1)
+
+        saveSegmentation_button = wx.Button(self, label="Save segmentation")
+        sizerV2.Add(saveSegmentation_button, flag=wx.SHAPED, proportion=1)
+
+        self.modelChoices = ['SEM', 'TEM', 'other']
+        self.modelCombobox = wx.ComboBox(self, choices=self.modelChoices, size=(100, 20), value='Select the modality')
+        sizerV3.Add(self.modelCombobox, flag=wx.SHAPED, proportion=1)
 
         applyModel_button = wx.Button(self, label = 'Apply ADS prediction model')
         applyModel_button.Bind(wx.EVT_BUTTON, self.onApplyModel_button)
-        sizer.Add(applyModel_button, flag=wx.SHAPED, proportion=1)
+        sizerV3.Add(applyModel_button, flag=wx.SHAPED, proportion=1)
+
+        fillMyelin_button = wx.Button(self, label='Fill closed myelin')
+        sizerV4.Add(fillMyelin_button, flag=wx.SHAPED, proportion=1)
+
+        self.SetSizer(sizerH)
+
+        self.imageDirPath = None
 
 
 
@@ -61,18 +94,25 @@ class Ads_control_v2(ctrlpanel.ControlPanel):
             print('Please load a PNG file')
             return
 
-        # Ask the user where the model is located
-        # Ask the user which file he wants to convert
-        with wx.DirDialog(self, "select the directory in which the model is locatted", defaultPath="",
-                           style=wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST) as fileDialog:
+        selectedModel = self.modelCombobox.GetStringSelection()
 
-            if fileDialog.ShowModal() == wx.ID_CANCEL:
-                return
+        if selectedModel == 'other':
+            # Ask the user where the model is located
+            with wx.DirDialog(self, "select the directory in which the model is locatted", defaultPath="",
+                              style=wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST) as fileDialog:
 
-        modelPath = fileDialog.GetPath()
+                if fileDialog.ShowModal() == wx.ID_CANCEL:
+                    return
 
+            modelPath = fileDialog.GetPath()
 
+        elif (selectedModel == 'SEM') or (selectedModel == 'TEM'):
+            modelPath = os.path.dirname(AxonDeepSeg.__file__)
+            modelPath = os.path.join(modelPath, 'models', 'default_' + selectedModel + '_model_v1')
 
+        else:
+            print('Please select a model')
+            return
 
 
         model_configfile = os.path.join(modelPath, 'config_network.json')
@@ -82,13 +122,15 @@ class Ads_control_v2(ctrlpanel.ControlPanel):
         prediction = axon_segmentation([self.imageDirPath], [self.pngImageName], modelPath,
                                        config_network, verbosity_level=3)
 
-        print('work_in_progress')
-        maskFilePath = self.imageDirPath + '/AxonDeepSeg.png'
 
-        self.loadPngImageFromPath(maskFilePath)
+        axonMaskPath = self.imageDirPath + '/AxonDeepSeg_seg-axon.png'
+        myelinMaskPath = self.imageDirPath + '/AxonDeepSeg_seg-myelin.png'
+
+        self.loadPngImageFromPath(axonMaskPath, isMask=True)
+        self.loadPngImageFromPath(myelinMaskPath, isMask=True)
 
 
-    def loadPngImageFromPath(self, imagePath):
+    def loadPngImageFromPath(self, imagePath, isMask=False):
         img_png = np.asarray(Image.open(imagePath).convert('LA'), dtype=np.uint8)
 
         if np.size(img_png.shape) == 3:
@@ -102,6 +144,9 @@ class Ads_control_v2(ctrlpanel.ControlPanel):
             print("Invalid image dimensions")
             return
 
+        if isMask is True:
+            img_png2D = img_png2D // 255
+
         img_NIfTI = nib.Nifti1Image(np.flipud(np.rot90(img_png2D, k=3, axes=(0, 1))), np.eye(4))
 
         # Save the NIfTI image
@@ -110,6 +155,26 @@ class Ads_control_v2(ctrlpanel.ControlPanel):
 
         img_overlay = ovLoad.loadOverlays(paths=[outFile], inmem=True, blocking=True)[0]
         self.overlayList.append(img_overlay)
+
+    def getCitation(self):
+
+        return ('If you use this work in your research, please cite it as follows: \n'
+                'Zaimi, A., Wabartha, M., Herman, V., Antonsanti, P.-L., Perone, C. S., & Cohen-Adad, J. (2018). '
+                'AxonDeepSeg: automatic axon and myelin segmentation from microscopy data using convolutional '
+                'neural networks. Scientific Reports, 8(1), 3816. '
+                'Link to paper: https://doi.org/10.1038/s41598-018-22181-4. \n'
+                'Copyright (c) 2018 NeuroPoly (Polytechnique Montreal)')
+
+    def getLogo(self):
+        path = os.path.dirname(AxonDeepSeg.__file__)
+
+        logoFile = path + '/ADS_logo.png'
+        png = wx.Image(logoFile,
+                       wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        png.SetSize((png.GetWidth()//3, png.GetHeight()//3))
+        logoImage = wx.StaticBitmap(self, -1, png, wx.DefaultPosition,
+                                    (png.GetWidth(), png.GetHeight()))
+        return logoImage
 
 
 
