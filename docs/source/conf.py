bbb40672a7bb5af5376f3b603dd3d89b3d240db2
Mathieu Boudreau <emb6150@gmail.com>
2019-03-12 16:48:23 -0230

    Update logo in RTD config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,7 +102,7 @@ html_theme_options = {
     'display_version': False,
     'navigation_depth': 4,
 }
-html_logo = '_static/logo_ads-alpha.png'
+html_logo = '_static/logo_ads-alpha-opaque-colors.png'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
