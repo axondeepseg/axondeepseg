9289ec5ee0b3b594cb8a97fceffba60f3e9a3037
Antoine Moevus <polymtlmaf88@gmail.com>
2019-03-18 15:25:33 -0400

    test_launch_morph[...]:  Refactor to use pathlib

--- test/morphometrics/test_launch_morphometrics_computation.py
+++ test/morphometrics/test_launch_morphometrics_computation.py
@@ -1,23 +1,20 @@
 # coding: utf-8
 
-import pytest
-import os
+from pathlib import Path
 import random
 import string
+import pytest
 
 from AxonDeepSeg.morphometrics.launch_morphometrics_computation import *
 
 
 class TestCore(object):
     def setup(self):
-        self.fullPath = os.path.dirname(os.path.abspath(__file__))
+        # Get the directory where this current file is saved
+        self.fullPath = Path(__file__).resolve().parent
         # Move up to the test directory, "test/"
-        self.testPath = os.path.split(self.fullPath)[0]
-        self.dataPath = os.path.join(
-            self.testPath,
-            '__test_files__',
-            '__test_demo_files__'
-            )
+        self.testPath = self.fullPath.parent
+        self.dataPath = self.testPath / '__test_files__' / '__test_demo_files__'
 
     def teardown(self):
         pass
@@ -30,24 +27,20 @@ class TestCore(object):
                          'axonlist.npy'
                          }
 
-        pathImg = os.path.join(self.dataPath, 'image.png')
-        pathPrediction = os.path.join(self.dataPath,
-                                      'AxonDeepSeg_seg-axonmyelin.png')
+        pathImg = self.dataPath / 'image.png'
+        pathPrediction = self.dataPath / 'AxonDeepSeg_seg-axonmyelin.png'
 
         launch_morphometrics_computation(pathImg, pathPrediction)
 
         for fileName in expectedFiles:
-            fullFilePath = os.path.join(self.dataPath, fileName)
-            assert os.path.isfile(fullFilePath)
-            os.remove(fullFilePath)
+            fullFilePath = self.dataPath / fileName
+            assert fullFilePath.is_file()
+            fullFilePath.unlink()
 
     @pytest.mark.unit
     def test_launch_morphometrics_computation_errors_for_missing_file(self):
         nonExistingFile = ''.join(random.choice(string.ascii_lowercase) for i in range(16))
-        pathPrediction = os.path.join(
-            self.dataPath,
-            'AxonDeepSeg_seg-axonmyelin.png'
-            )
+        pathPrediction = self.dataPath / 'AxonDeepSeg_seg-axonmyelin.png'
 
         with pytest.raises((IOError, OSError)):
             launch_morphometrics_computation(nonExistingFile, pathPrediction)
