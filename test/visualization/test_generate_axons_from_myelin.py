58124e9dc96552ded32cef59ba08bb4a74903b84
Antoine Moevus <polymtlmaf88@gmail.com>
2019-03-19 10:57:50 -0400

    test_generate_axons_from_myelin: refact to pathlib

--- test/visualization/test_generate_axons_from_myelin.py
+++ test/visualization/test_generate_axons_from_myelin.py
@@ -1,55 +1,38 @@
 # coding: utf-8
 
-import pytest
-import os
+from pathlib import Path
 import numpy as np
 from scipy.misc import imread
+
+import pytest
 
 from AxonDeepSeg.visualization.generate_axons_from_myelin import *
 
 
 class TestCore(object):
     def setup(self):
-        self.fullPath = os.path.dirname(os.path.abspath(__file__))
-
+        # Get the directory where this current file is saved
+        self.fullPath = Path(__file__).resolve().parent
         # Move up to the test directory, "test/"
-        self.testPath = os.path.split(self.fullPath)[0]
+        self.testPath = self.fullPath.parent
 
-        self.folderPath = os.path.join(
-            self.testPath,
-            '__test_files__',
-            '__test_demo_files__'
-            )
+        self.folderPath = self.testPath / '__test_files__'/ '__test_demo_files__'
 
-        self.axonMyelinMask = os.path.join(
-            self.folderPath,
-            'AxonDeepSeg_seg-axonmyelin.png'
-            )
+        self.axonMyelinMask = self.folderPath / 'AxonDeepSeg_seg-axonmyelin.png'
 
-        self.myelinMask = os.path.join(
-            self.folderPath,
-            'AxonDeepSeg_seg-myelin.png'
-            )
+        self.myelinMask = self.folderPath / 'AxonDeepSeg_seg-myelin.png'
 
     def teardown(self):
-        if os.path.isfile(os.path.join(self.folderPath, 'axon_myelin_mask_corrected.png')):
-
-            os.remove(
-                os.path.join(self.folderPath, 'axon_myelin_mask_corrected.png')
-                )
+        if (self.folderPath / 'axon_myelin_mask_corrected.png').is_file():
+            (self.folderPath / 'axon_myelin_mask_corrected.png').unlink()
 
     # --------------generate_axons_from_myelin tests-------------- #
     @pytest.mark.unit
     def test_generate_axons_from_myelin_creates_expected_file(self):
 
-        if os.path.isfile(os.path.join(self.folderPath, 'axon_myelin_mask_corrected.png')):
-
-            os.remove(
-                os.path.join(self.folderPath, 'axon_myelin_mask_corrected.png')
-                )
+        if (self.folderPath / 'axon_myelin_mask_corrected.png').is_file():
+                (self.folderPath / 'axon_myelin_mask_corrected.png').unlink()
 
         generate_axons_from_myelin(self.axonMyelinMask, self.myelinMask)
-        
-        assert os.path.isfile(
-            os.path.join(self.folderPath, 'axon_myelin_mask_corrected.png')
-            )
+
+        assert (self.folderPath / 'axon_myelin_mask_corrected.png').is_file()
