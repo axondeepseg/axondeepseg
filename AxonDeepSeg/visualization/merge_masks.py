f0633d97dca30d7a8797d8064404394184aac74b
Antoine Moevus <polymtlmaf88@gmail.com>
2019-03-21 10:28:24 -0400

    merge_masks.py: use pathlib

--- AxonDeepSeg/visualization/merge_masks.py
+++ AxonDeepSeg/visualization/merge_masks.py
@@ -1,19 +1,21 @@
+
+from pathlib import Path
 import numpy as np
-import os
 import imageio
+
 import AxonDeepSeg.ads_utils
 
-def merge_masks(path_axon,path_myelin):
+def merge_masks(path_axon, path_myelin):
 
     axon = imageio.imread(path_axon)
     myelin = imageio.imread(path_myelin)
-    
+
     both = (axon/255)*255 + (myelin/255)*127
-    
+
     # get main path
-    path_folder, file_name = os.path.split(path_axon)
+    path_folder = path_axon.parent
 
     # save the masks
-    imageio.imwrite(os.path.join(path_folder,'axon_myelin_mask.png'),both)
+    imageio.imwrite(path_folder / 'axon_myelin_mask.png', both)
 
     return both
