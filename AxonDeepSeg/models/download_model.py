9f71dba8c5591f78ef56d311d4e7a10486951803
vs74 <vasudevsharma74@yahoo.com>
2019-09-20 19:30:11 +0530

    Revert to old convention(#240)

---
+++ AxonDeepSeg/models/download_model.py
@@ -0,0 +1,16 @@
+from AxonDeepSeg.ads_utils import download_data
+
+def main():
+    url_TEM_model = "https://osf.io/bxq9v/?action=download"  # URL of TEM model hosted on OSF storage
+    url_SEM_model = "https://osf.io/scwbk/?action=download"  # URL of SEM model hosted on OSF storage
+
+    if (not download_data(url_TEM_model) and not download_data(url_SEM_model)) ==1:
+        print('Data downloaded and unzipped succesfully.')
+    else:
+        print('ERROR: Data was not succesfully downloaded and unzipped- please check your link and filename and try again.')
+
+
+
+# Calling the script
+if __name__ == '__main__':
+    main()
