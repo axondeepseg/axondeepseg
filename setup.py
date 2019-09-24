54196ac7bb8bb74d90f23b3a61367d8e6656f2fa
Vasudev Sharma <vasudevsharma74@yahoo.com>
2019-09-24 21:20:25 +0530

    Update setup.py
    Remove unnecessary import (#240)

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
-from setuptools.command.install import install
 from subprocess import check_call
 
 from codecs import open
