fb3f5110ecf6082a8617a9869d57eb996f70362b
Stoyan I. Asenov <43790661+Stoyan-I-A@users.noreply.github.com>
2020-07-10 01:08:04 -0400

    #321 added the remove_intersection function

--- AxonDeepSeg/postprocessing.py
+++ AxonDeepSeg/postprocessing.py
@@ -57,3 +57,28 @@ def floodfill_axons(axon_array, myelin_array):
     )
     axon_extracted_array = axon_extracted_array.astype(np.uint8)
     return axon_extracted_array
+
+def remove_intersection(mask_1, mask_2, priority=1):
+    """
+    This function removes the overlap between two masks on one of those two masks depending on the priority parameter.
+    :param mask_1: First mask, with an intersecting section with the sectond one
+    :param mask_2: Second mask, with an intersecting section with the first one
+    :param priority: Tells which of the two arrays should be kept. Can only be 1 or 2.
+    :type priority: int
+    :return split_mask_1: First mask, minus the intersection if it doesn't have priority
+    :return split_mask_2: Sectond mask, minus the intersection if it doesn't have priority
+    """
+
+    if priority not in [1, 2]:
+        raise Exception("Parameter priority can only be 1 or 2")
+
+    array_1 = mask_1.astype(np.bool)
+    array_2 = mask_2.astype(np.bool)
+    intersection = (array_1 & array_2).astype(np.uint8)
+
+    if priority is 1:
+        mask_2 = mask_2 - intersection
+    if priority is 2:
+        mask_1 = mask_1 - intersection
+
+    return mask_1, mask_2
