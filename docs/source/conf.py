6e77e0d5694b56a0a05a5b63bb62acf225e7898b
Marie-Helene Bourget <bourgetmarieh@gmail.com>
2020-11-09 18:29:32 -0500

    Added recommonmark to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,9 @@ import AxonDeepSeg
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.githubpages']
+extensions = [
+	'sphinx.ext.githubpages',
+	'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
