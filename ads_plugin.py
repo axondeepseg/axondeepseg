8d0f0e39b71c621065efbaa3c0e63fbe713ba449
Stoyan I. Asenov <43790661+Stoyan-I-A@users.noreply.github.com>
2020-11-22 16:16:26 -0500

    #401 the ads_plugin now uses the fill_myelin_holes function when pressing the fill axons button

--- ads_plugin.py
+++ ads_plugin.py
@@ -35,7 +35,7 @@ import imageio
 
 from AxonDeepSeg.morphometrics.compute_morphometrics import *
 
-VERSION = "0.2.14"
+VERSION = "0.2.15"
 
 class ADScontrol(ctrlpanel.ControlPanel):
     """
@@ -494,19 +494,15 @@ class ADScontrol(ctrlpanel.ControlPanel):
         """
         # Find the visible myelin and axon mask
         myelin_mask_overlay = self.get_visible_myelin_overlay()
-        axon_mask_overlay = self.get_visible_axon_overlay()
 
         if myelin_mask_overlay is None:
-            return
-        if axon_mask_overlay is None:
             return
 
         # Extract the data from the overlays
         myelin_array = myelin_mask_overlay[:, :, 0]
-        axon_array = axon_mask_overlay[:, :, 0]
 
         # Perform the floodfill operation
-        axon_extracted_array = postprocessing.floodfill_axons(axon_array, myelin_array)
+        axon_extracted_array = postprocessing.fill_myelin_holes(myelin_array)
 
         axon_corr_array = np.flipud(axon_extracted_array)
         axon_corr_array = params.intensity['binary'] * np.rot90(axon_corr_array, k=1, axes=(1, 0))
