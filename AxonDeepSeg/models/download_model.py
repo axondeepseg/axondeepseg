38e2efb4f825edcaabf219cad7607769ee660de5
Vasudev Sharma <vasudevsharma74@yahoo.com>
2021-03-02 05:12:29 +0530

    Integrate Simeon's model with FSLeyes and CLI (#457)
    * #442 started replacing os.path with pathlib
    
    * #442 Finished refactoring paths
    
    * Download Simeon's model from OSF
    
    * Revert  to the master branch
    
    * Revert `ads_plugin` to master
    
    * Update ads_plugin.py
    
    * Update ads_plugin.py
    
    * Download Simeon's model
    
    * Add tests and fix download errors
    
    * Fix download errors
    
    * Fix comments
    
    * Integrate Simeon's model with CLI
    
    * Update version of the model
    
    * Fix travis build and update model version
    
    * Fix typo's
    
    Co-authored-by: Stoyan I. Asenov <43790661+Stoyan-I-A@users.noreply.github.com>
    Co-authored-by: Mathieu Boudreau <emb6150@gmail.com>

--- AxonDeepSeg/models/download_model.py
+++ AxonDeepSeg/models/download_model.py
@@ -7,16 +7,19 @@ def download_model(destination = None):
     if destination is None:
         sem_destination = Path("AxonDeepSeg/models/default_SEM_model")
         tem_destination = Path("AxonDeepSeg/models/default_TEM_model")
+        model_seg_pns_bf_destination = Path("AxonDeepSeg/models/model_seg_pns_bf")
     else:
         destination = convert_path(destination)
         sem_destination = destination / "default_SEM_model"
         tem_destination = destination / "default_TEM_model"
+        model_seg_pns_bf_destination = destination / "model_seg_pns_bf"
 
     url_TEM_model = "https://osf.io/2hcfv/?action=download&version=5"  # URL of TEM model hosted on OSF storage with the appropriate versioning on OSF
     url_SEM_model = "https://osf.io/sv7u2/?action=download&version=5"  # URL of SEM model hosted on OSF storage with the appropriate versioning on OSF
+    url_model_seg_pns_bf = "https://osf.io/hfeu2/?action=download&version=2" # URL of Bright Field Optical Microscopy model hosted on storage with the appropriate versioning on OSF
 
     if (
-        not download_data(url_TEM_model) and not download_data(url_SEM_model)
+        not download_data(url_TEM_model) and not download_data(url_SEM_model) and not download_data(url_model_seg_pns_bf)
     ) == 1:
         print("Data downloaded and unzipped succesfully.")
     else:
@@ -30,9 +33,13 @@ def download_model(destination = None):
     if tem_destination.exists():
         print('TEM model folder already existed - deleting old one.')
         shutil.rmtree(str(tem_destination))
+    if model_seg_pns_bf_destination.exists():
+        print('Bright Field Optical Microscopy model folder already existed - deleting old one')
+        shutil.rmtree(str(model_seg_pns_bf_destination))
 
     shutil.move("default_SEM_model", str(sem_destination))
     shutil.move("default_TEM_model", str(tem_destination))
+    shutil.move("model_seg_pns_bf", str(model_seg_pns_bf_destination))
 
 
 def main(argv=None):
