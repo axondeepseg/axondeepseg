2443e86a7ad12a8037134bdc020d43312f81705d
vs74 <vasudevsharma74@yahoo.com>
2021-03-12 23:28:30 +0530

    Change default name

--- AxonDeepSeg/morphometrics/launch_morphometrics_computation.py
+++ AxonDeepSeg/morphometrics/launch_morphometrics_computation.py
@@ -80,7 +80,7 @@ def main(argv=None):
     ap.add_argument('-i', '--imgpath', required=True, nargs='+', help='Path to the image.')
 
     ap.add_argument('-f', '--filename', required=False,  help='Name of the excel file in which the morphometrics will be stored',
-                                                              default="axon_morphometrics"  )
+                                                              default="axon_morphometrics")
 
     ap.add_argument('-a', '--axonshape', required=False, help='Axon shape: circle \n' +
                                                                           '\t    ellipse \n' +
