be681dfa1808e941596c1952fcec0958aefc63b1
Vasudev Sharma <vasudevsharma74@yahoo.com>
2021-04-15 20:26:41 +0530

    Tests: remove axon shape

--- test/morphometrics/test_launch_morphometrics_computation.py
+++ test/morphometrics/test_launch_morphometrics_computation.py
@@ -20,7 +20,6 @@ class TestCore(object):
         self.testPath = self.fullPath.parent
         self.dataPath = self.testPath / '__test_files__' / '__test_demo_files__'
 
-        self.axon_shape = "ellipse"         # axon shape is set to ellipse
         self.morphometricsFile = "axon_morphometrics.xlsx"
         self.morphometricsPath = self.dataPath / self.morphometricsFile
 
@@ -78,7 +77,7 @@ class TestCore(object):
         pathImg = self.dataPath / 'image.png'
 
         with pytest.raises(SystemExit) as pytest_wrapped_e:
-            AxonDeepSeg.morphometrics.launch_morphometrics_computation.main(["-s", "0.07", "-i", str(pathImg), "-f", "axon_morphometrics", "-a", "circle"])
+            AxonDeepSeg.morphometrics.launch_morphometrics_computation.main(["-s", "0.07", "-i", str(pathImg), "-f", "axon_morphometrics"])
 
         assert (pytest_wrapped_e.type == SystemExit) and (pytest_wrapped_e.value.code == 0) and self.morphometricsPath.exists()
     
@@ -99,24 +98,6 @@ class TestCore(object):
 
         with pytest.raises(SystemExit) as pytest_wrapped_e:
             AxonDeepSeg.morphometrics.launch_morphometrics_computation.main(["-i", str(pathImg), '-f', 'axon_morphometrics.csv'])
-
-        assert (pytest_wrapped_e.type == SystemExit) and (pytest_wrapped_e.value.code == 0) and self.morphometricsPath.exists()
-
-    @pytest.mark.unit
-    def test_main_cli_runs_succesfully_with_valid_inputs_with_axon_shape_as_ellipse(self):
-        pathImg = self.dataPath / 'image.png'
-
-        with pytest.raises(SystemExit) as pytest_wrapped_e:
-            AxonDeepSeg.morphometrics.launch_morphometrics_computation.main(["-i", str(pathImg), "-a", "ellipse"])
-
-        assert (pytest_wrapped_e.type == SystemExit) and (pytest_wrapped_e.value.code == 0) and self.morphometricsPath.exists()
-    
-    @pytest.mark.unit
-    def test_main_cli_runs_succesfully_with_valid_inputs_with_axon_shape_as_circle(self):
-        pathImg = self.dataPath / 'image.png'
-
-        with pytest.raises(SystemExit) as pytest_wrapped_e:
-            AxonDeepSeg.morphometrics.launch_morphometrics_computation.main(["-i", str(pathImg), "-a", "circle"])
 
         assert (pytest_wrapped_e.type == SystemExit) and (pytest_wrapped_e.value.code == 0) and self.morphometricsPath.exists()
 
