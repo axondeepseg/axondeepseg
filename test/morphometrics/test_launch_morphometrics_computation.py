dc725deed525ca760f63ccc9d91c70419a46b518
Vasudev Sharma <vasudevsharma74@yahoo.com>
2021-04-15 20:32:42 +0530

    Tests: Remove ellipse as axon shape

--- test/morphometrics/test_launch_morphometrics_computation.py
+++ test/morphometrics/test_launch_morphometrics_computation.py
@@ -47,23 +47,6 @@ class TestCore(object):
             fullFilePath.unlink()
 
     @pytest.mark.unit
-    def test_launch_morphometrics_computation_saves_expected_files_with_axon_as_ellipse(self):
-        expectedFiles = {'aggregate_morphometrics.txt',
-                         'AxonDeepSeg_map-axondiameter.png',
-                         'axonlist.npy'
-                         }
-
-        pathImg = self.dataPath / 'image.png'
-        pathPrediction = self.dataPath / ('image' + str(axonmyelin_suffix))
-
-        launch_morphometrics_computation(str(pathImg), str(pathPrediction), axon_shape=self.axon_shape)
-
-        for fileName in expectedFiles:
-            fullFilePath = self.dataPath / fileName
-            assert fullFilePath.is_file()
-            fullFilePath.unlink()
-
-    @pytest.mark.unit
     def test_launch_morphometrics_computation_errors_for_missing_file(self):
         nonExistingFile = ''.join(random.choice(string.ascii_lowercase) for i in range(16))
         pathPrediction = self.dataPath / ('image' + str(axonmyelin_suffix))
