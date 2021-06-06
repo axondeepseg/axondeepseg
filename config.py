109bff078f353b1fa24c6e6fcf6b83a0bf0f9ed1
stoyan-I-A <sto400@hotmail.com>
2021-06-06 15:07:37 -0400

    #519 added suffixes to the config.py file

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ from pathlib import Path
 axonmyelin_suffix = Path('_seg-axonmyelin.png') # axon + myelin segmentation suffix file name
 axon_suffix = Path('_seg-axon.png')             # axon segmentation suffix file name
 myelin_suffix = Path('_seg-myelin.png')         # myelin segmentation suffix file name
+index_suffix = Path('_IMAGE_index.png')
+axonmyelin_index_suffix = Path('_AXONMYELIN_index.png')
