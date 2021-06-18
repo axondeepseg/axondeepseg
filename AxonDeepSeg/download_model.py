8b3ceca6c8f668ee2b158cc1e0a8d5689f389729
Armand Collin <armac@videotron.ca>
2021-06-18 11:16:55 -0400

    added fetching latest released model from github

--- AxonDeepSeg/download_model.py
+++ AxonDeepSeg/download_model.py
@@ -1,6 +1,7 @@
 from AxonDeepSeg.ads_utils import convert_path, download_data
 from pathlib import Path
 import shutil
+import requests
 
 
 def download_model(destination = None):
@@ -14,9 +15,17 @@ def download_model(destination = None):
         tem_destination = destination / "default_TEM_model"
         model_seg_pns_bf_destination = destination / "model_seg_pns_bf"
 
-    url_TEM_model = "https://osf.io/2hcfv/?action=download&version=5"  # URL of TEM model hosted on OSF storage with the appropriate versioning on OSF
-    url_SEM_model = "https://osf.io/sv7u2/?action=download&version=5"  # URL of SEM model hosted on OSF storage with the appropriate versioning on OSF
-    url_model_seg_pns_bf = "https://osf.io/hfeu2/?action=download&version=2" # URL of Bright Field Optical Microscopy model hosted on storage with the appropriate versioning on OSF
+    # retrieve latest release identifier from github API
+    response_TEM = requests.get("https://api.github.com/repos/axondeepseg/default-TEM-model/releases/latest")
+    folder_name_TEM_model = Path("default-TEM-model-"+str(response_TEM.json()["tag_name"]))
+    response_SEM = requests.get("https://api.github.com/repos/axondeepseg/default-SEM-model/releases/latest")
+    folder_name_SEM_model = Path("default-SEM-model-"+str(response_SEM.json()["tag_name"]))
+    response_OM = requests.get("https://api.github.com/repos/axondeepseg/model-seg-pns-bf/releases/latest")
+    folder_name_OM_model = Path("model-seg-pns-bf-"+str(response_OM.json()["tag_name"]))
+
+    url_TEM_model = "https://github.com/axondeepseg/default-TEM-model/archive/refs/tags/"+str(response_TEM.json()["tag_name"])+".zip" 
+    url_SEM_model = "https://github.com/axondeepseg/default-SEM-model/archive/refs/tags/"+str(response_SEM.json()["tag_name"])+".zip" 
+    url_model_seg_pns_bf = "https://github.com/axondeepseg/model-seg-pns-bf/archive/refs/tags/"+str(response_OM.json()["tag_name"])+".zip"
 
     if (
         not download_data(url_TEM_model) and not download_data(url_SEM_model) and not download_data(url_model_seg_pns_bf)
@@ -37,10 +46,14 @@ def download_model(destination = None):
         print('Bright Field Optical Microscopy model folder already existed - deleting old one')
         shutil.rmtree(str(model_seg_pns_bf_destination))
 
-    shutil.move("default_SEM_model", str(sem_destination))
-    shutil.move("default_TEM_model", str(tem_destination))
-    shutil.move("model_seg_pns_bf", str(model_seg_pns_bf_destination))
+    shutil.move(folder_name_SEM_model.joinpath("default_SEM_model"), str(sem_destination))
+    shutil.move(folder_name_TEM_model.joinpath('default_TEM_model'), str(tem_destination))
+    shutil.move(folder_name_OM_model.joinpath("model_seg_pns_bf"), str(model_seg_pns_bf_destination))
 
+    # remove temporary folders
+    shutil.rmtree(folder_name_TEM_model)
+    shutil.rmtree(folder_name_SEM_model)
+    shutil.rmtree(folder_name_OM_model)
 
 def main(argv=None):
     download_model()
