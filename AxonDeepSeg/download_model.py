25ca9ddd58a9e0a9f989e6e34212cc998ffc603f
Armand <83031821+hermancollin@users.noreply.github.com>
2021-06-23 15:12:08 -0400

    Download models from Github repo (#533)
    * added fetching latest released model from github
    
    * fixed quotation marks
    
    * changed json syntax for mac os x compatibility
    
    * added specific tag names in URLs
    
    * changed os.listdir() for pathlib's iterdir()
    
    Co-authored-by: Vasudev Sharma <vasudevsharma74@yahoo.com>

--- AxonDeepSeg/download_model.py
+++ AxonDeepSeg/download_model.py
@@ -14,9 +14,11 @@ def download_model(destination = None):
         tem_destination = destination / "default_TEM_model"
         model_seg_pns_bf_destination = destination / "model_seg_pns_bf"
 
-    url_TEM_model = "https://osf.io/2hcfv/?action=download&version=5"  # URL of TEM model hosted on OSF storage with the appropriate versioning on OSF
-    url_SEM_model = "https://osf.io/sv7u2/?action=download&version=5"  # URL of SEM model hosted on OSF storage with the appropriate versioning on OSF
-    url_model_seg_pns_bf = "https://osf.io/hfeu2/?action=download&version=2" # URL of Bright Field Optical Microscopy model hosted on storage with the appropriate versioning on OSF
+    url_TEM_model = "https://github.com/axondeepseg/default-TEM-model/archive/refs/tags/r20210615.zip" 
+    url_SEM_model = "https://github.com/axondeepseg/default-SEM-model/archive/refs/tags/r20210615.zip" 
+    url_model_seg_pns_bf = "https://github.com/axondeepseg/model-seg-pns-bf/archive/refs/tags/r20210615.zip"
+
+    files_before = list(Path.cwd().iterdir())
 
     if (
         not download_data(url_TEM_model) and not download_data(url_SEM_model) and not download_data(url_model_seg_pns_bf)
@@ -26,6 +28,14 @@ def download_model(destination = None):
         print(
             "ERROR: Data was not succesfully downloaded and unzipped- please check your link and filename and try again."
         )
+
+    files_after = list(Path.cwd().iterdir())
+
+    # retrieving unknown model folder names
+    model_folders = list(set(files_after)-set(files_before))
+    folder_name_TEM_model = ''.join([str(x) for x in model_folders if 'TEM' in str(x)])
+    folder_name_SEM_model = ''.join([str(x) for x in model_folders if 'SEM' in str(x)])
+    folder_name_OM_model = ''.join([str(x) for x in model_folders if 'pns-bf' in str(x)])
 
     if sem_destination.exists():
         print('SEM model folder already existed - deleting old one.')
@@ -37,10 +47,14 @@ def download_model(destination = None):
         print('Bright Field Optical Microscopy model folder already existed - deleting old one')
         shutil.rmtree(str(model_seg_pns_bf_destination))
 
-    shutil.move("default_SEM_model", str(sem_destination))
-    shutil.move("default_TEM_model", str(tem_destination))
-    shutil.move("model_seg_pns_bf", str(model_seg_pns_bf_destination))
+    shutil.move(Path(folder_name_SEM_model).joinpath("default_SEM_model"), str(sem_destination))
+    shutil.move(Path(folder_name_TEM_model).joinpath("default_TEM_model"), str(tem_destination))
+    shutil.move(Path(folder_name_OM_model).joinpath("model_seg_pns_bf"), str(model_seg_pns_bf_destination))
 
+    # remove temporary folders
+    shutil.rmtree(folder_name_TEM_model)
+    shutil.rmtree(folder_name_SEM_model)
+    shutil.rmtree(folder_name_OM_model)
 
 def main(argv=None):
     download_model()
