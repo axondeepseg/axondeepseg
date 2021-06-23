2d58d307f092949329eddae1720f13d8bb2310b7
vs74 <sharmavasudev1998@gmail.com>
2021-06-23 23:07:05 +0530

    Add tag name for retro compatibility

--- AxonDeepSeg/download_tests.py
+++ AxonDeepSeg/download_tests.py
@@ -1,21 +1,17 @@
 from AxonDeepSeg.ads_utils import download_data, convert_path
 from pathlib import Path
 import shutil
-import requests
 
-def download_tests(destination = None):
+
+def download_tests(destination=None):
     if destination is None:
         test_files_destination = Path("test/__test_files__")
     else:
         destination = convert_path(destination)
         test_files_destination = destination / "__test_files__"
 
-
-    # retrieve latest release identifier from github API
-    response_tests_files = requests.get("https://api.github.com/repos/axondeepseg/data-testing/releases/latest")
-    folder_name_test_files = Path("data-testing-"+str(response_tests_files.json()['tag_name']))
-
-    url_tests = "https://github.com/axondeepseg/data-testing/archive/refs/tags/"+str(response_tests_files.json()['tag_name'])+".zip"   # URL of TEM model hosted on OSF storage with the appropriate versioning on OSF
+    url_tests = "https://github.com/axondeepseg/data-testing/archive/refs/tags/r20210615.zip"  
+    files_before = list(Path.cwd().iterdir())
 
     if (
         not download_data(url_tests)
@@ -26,12 +22,21 @@ def download_tests(destination = None):
             "ERROR: Test data was not successfully downloaded and unzipped- please check your link and filename and try again."
         )
 
+    files_after = list(Path.cwd().iterdir())
+
+    # retrieving unknown test files names
+    test_folder = list(set(files_after)-set(files_before))
+    folder_name_test_files = ''.join([str(x) for x in test_folder if 'data-testing' in str(x)])
+
     if test_files_destination.exists():
-        print('SEM model folder already existed - deleting old one.')
-        shutil.rmtree(str(folder_name_test_files))
+        print('Test files folder already existed - deleting old one.')
+        shutil.rmtree(str(test_files_destination))
 
-    shutil.move(folder_name_test_files.joinpath("__test_files__"), str(test_files_destination))
+    print(str(Path(folder_name_test_files).joinpath("__test_files__")))
+    shutil.move(Path(folder_name_test_files).joinpath("__test_files__"), str(test_files_destination))
 
+    # remove temporary folder
+    shutil.rmtree(folder_name_test_files)
 
 def main(argv=None):
     download_tests()
