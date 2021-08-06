cc3ce427a2f5b832ec0083b1d6dec15c3c078f8b
Mathieu Boudreau <emb6150@gmail.com>
2021-08-06 15:57:07 -0300

    Save merge mask

--- AxonDeepSeg/apply_model.py
+++ AxonDeepSeg/apply_model.py
@@ -1,5 +1,6 @@
 # AxonDeepSeg imports
 import AxonDeepSeg.ads_utils as ads
+from AxonDeepSeg.visualization import merge_masks
 from config import axonmyelin_suffix
 
 from ivadomed import inference as imed_inference
@@ -19,4 +20,6 @@ def axon_segmentation(
 
     nii_lst, target_lst = imed_inference.segment_volume(str(path_model), input_filenames, options=options)
 
-    imed_inference.pred_to_png(nii_lst, target_lst, path_acquisitions_folders)
+    imed_inference.pred_to_png(nii_lst, target_lst, str(Path(input_filenames[0]).parent / Path(input_filenames[0]).stem))
+
+    merge_masks(Path(path_acquisitions_folders) / 'image_seg-axon-manual_pred.png', Path(path_acquisitions_folders) / 'image_seg-myelin-manual_pred.png', Path(path_acquisitions_folders) / (Path(input_filenames[0]).stem) + str(axonmyelin_suffix)))
