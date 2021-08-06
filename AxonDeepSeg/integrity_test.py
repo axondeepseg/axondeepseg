a42cee897b29699a70eea80f1071ea4096f68d5b
Mathieu Boudreau <emb6150@gmail.com>
2021-08-06 16:10:57 -0300

    Debug

--- AxonDeepSeg/integrity_test.py
+++ AxonDeepSeg/integrity_test.py
@@ -33,9 +33,10 @@ def integrity_test():
         # Launch the axon and myelin segmentation on test image sample provided in the installation
         print('Computing the segmentation of axon and myelin on test image.')
         axon_segmentation(path_testing, [str(path_testing / image)], path_model, overlap_value=[48,48], acquired_resolution=0.13)
-
+        print('0')
         # Read the ground truth mask and the obtained segmentation mask
         mask = ads.imread(path_testing / 'mask.png')
+        print('1')
         pred = ads.imread(path_testing / (image.stem + str(axonmyelin_suffix)))
 
         # Generate separate axon and myelin masks of the segmentation output
