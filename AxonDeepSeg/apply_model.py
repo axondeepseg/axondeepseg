e8ef3e0875f0b162747ce33df74ce7f0dbd480da
Mathieu Boudreau <emb6150@gmail.com>
2021-08-06 11:36:45 -0300

    Early implementation of segmentation wrapper

---
+++ AxonDeepSeg/apply_model.py
@@ -0,0 +1,22 @@
+# AxonDeepSeg imports
+import AxonDeepSeg.ads_utils as ads
+from config import axonmyelin_suffix
+
+from ivadomed import inference as imed_inference
+
+def axon_segmentation(
+                    path_acquisitions_folders, 
+                    acquisitions_filenames,
+                    path_model_folder,
+                    overlap_value=[48,48],
+                    acquired_resolution=None,
+                    segmentations_filenames=[str(axonmyelin_suffix)],
+                    ):
+
+    path_model=path_model_folder
+    input_filenames = acquisitions_filenames
+    options = {"pixel_size": acquired_resolution, "overlap_2D":overlap_value, "binarize_maxpooling": True}
+
+    nii_lst, target_lst = imed_inference.segment_volume(path_model, input_filenames, options=options)
+
+    imed_inference.pred_to_png(nii_lst, target_lst, path_acquisitions_folders)
