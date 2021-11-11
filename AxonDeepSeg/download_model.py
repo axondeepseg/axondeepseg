7e9aed9bee32b9709916e355cb8656eb8dd61f53
Mathieu Boudreau <emb6150@gmail.com>
2021-11-11 10:15:24 -0400

    Update SEM model

--- AxonDeepSeg/download_model.py
+++ AxonDeepSeg/download_model.py
@@ -5,16 +5,16 @@ import shutil
 
 def download_model(destination = None):
     if destination is None:
-        model_seg_rat_axon_myelin_sem = Path("AxonDeepSeg/models/model_seg_rat_axon-myelin_sem")
+        sem_destination = Path("AxonDeepSeg/models/default_SEM_model")
     else:
-        model_seg_rat_axon_myelin_sem = destination / "model_seg_rat_axon-myelin_sem"
+        sem_destination = destination / "default_SEM_model"
 
-    url_model_seg_rat_axon_myelin_sem = "https://github.com/axondeepseg/model_seg_rat_axon-myelin_sem/archive/refs/tags/r20210806.zip"
+    url_sem_destination = "https://github.com/axondeepseg/default_SEM_model/archive/refs/tags/r20211111.zip"
 
     files_before = list(Path.cwd().iterdir())
 
     if (
-        not download_data(url_model_seg_rat_axon_myelin_sem)
+        not download_data(url_sem_destination)
     ) == 1:
         print("Data downloaded and unzipped succesfully.")
     else:
@@ -26,16 +26,16 @@ def download_model(destination = None):
 
     # retrieving unknown model folder names
     model_folders = list(set(files_after)-set(files_before))
-    folder_name_SEM_ivadomed_model = ''.join([str(x) for x in model_folders if 'rat_axon' in str(x)])
+    folder_name_SEM_model = ''.join([str(x) for x in model_folders if 'rat_axon' in str(x)])
 
-    if model_seg_rat_axon_myelin_sem.exists():
-        print('IVADOMED SEM model folder already existed - deleting old one')
-        shutil.rmtree(str(model_seg_rat_axon_myelin_sem))
+    if sem_destination.exists():
+        print('SEM model folder already existed - deleting old one')
+        shutil.rmtree(str(sem_destination))
         
-    shutil.move(Path(folder_name_SEM_ivadomed_model).joinpath("model_seg_rat_axon-myelin_sem"), str(model_seg_rat_axon_myelin_sem))
+    shutil.move(Path(sem_destination).joinpath("model_seg_rat_axon-myelin_sem"), str(sem_destination))
 
     # remove temporary folders
-    shutil.rmtree(folder_name_SEM_ivadomed_model)
+    shutil.rmtree(folder_name_SEM_model)
 
 def main(argv=None):
     download_model()
