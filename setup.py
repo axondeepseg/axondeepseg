59a4d91e716b6995beeac07c60b93ecbafed4a7b
Mathieu Boudreau <emb6150@gmail.com>
2022-01-12 11:31:12 -0400

    Update python version in setup classifier

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
     ],
 
     keywords='',
