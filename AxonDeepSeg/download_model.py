f46bbda1e994956a6bcdfcb927b57e7cae32ecbc
Mathieu Boudreau <emb6150@gmail.com>
2023-02-11 00:35:46 -0500

    Bring up to date with Vasudevs original PR

--- AxonDeepSeg/download_model.py
+++ AxonDeepSeg/download_model.py
@@ -1,7 +1,8 @@
 from AxonDeepSeg.ads_utils import convert_path, download_data
 from pathlib import Path
 import shutil
-
+import argparse
+from argparse import RawTextHelpFormatter
 
 def download_model(destination = None):
     sem_release_version = 'r20211209v2'
@@ -9,17 +10,29 @@ def download_model(destination = None):
     bf_release_version = 'r20211210'
     
     if destination is None:
-        sem_destination = Path("AxonDeepSeg/models/model_seg_rat_axon-myelin_sem")
-        tem_destination = Path("AxonDeepSeg/models/model_seg_mouse_axon-myelin_tem")
-        bf_destination = Path("AxonDeepSeg/models/model_seg_rat_axon-myelin_bf")
+        sem_destination = Path(__file__).parent / "model_seg_rat_axon-myelin_sem"
+        tem_destination = Path(__file__).parent / "model_seg_mouse_axon-myelin_tem"
+        bf_destination = Path(__file__).parent / "model_seg_rat_axon-myelin_bf"
     else:
         sem_destination = destination / "model_seg_rat_axon-myelin_sem"
         tem_destination = destination / "model_seg_mouse_axon-myelin_tem"
         bf_destination = destination / "model_seg_rat_axon-myelin_bf"
 
+    model_working_directory = True if destination is not None and destination.name == Path.cwd().name else False # Flag to check if user wants to store model in the working directory or not
+    
     url_sem_destination = "https://github.com/axondeepseg/default-SEM-model/archive/refs/tags/" + sem_release_version + ".zip"
     url_tem_destination = "https://github.com/axondeepseg/default-TEM-model/archive/refs/tags/" + tem_release_version + ".zip"
     url_bf_destination = "https://github.com/axondeepseg/default-BF-model/archive/refs/tags/" + bf_release_version + ".zip"
+
+    if sem_destination.exists():                                                
+        print('SEM model folder already existed - deleting old one.')
+        shutil.rmtree(str(sem_destination))
+    if tem_destination.exists():
+        print('TEM model folder already existed - deleting old one.')
+        shutil.rmtree(str(tem_destination))
+    if bf_destination.exists():
+        print('BF model folder already existed - deleting old one.')
+        shutil.rmtree(str(bf_destination))
 
     files_before = list(Path.cwd().iterdir())
 
@@ -31,6 +44,11 @@ def download_model(destination = None):
         print(
             "ERROR: Data was not succesfully downloaded and unzipped- please check your link and filename and try again."
         )
+
+    if not model_working_directory:           # if destination folder is other than root directory
+        shutil.move("model_seg_rat_axon-myelin_sem", str(sem_destination))
+        shutil.move("model_seg_mouse_axon-myelin_tem", str(tem_destination))
+        shutil.move("model_seg_rat_axon-myelin_bf", str(bf_destination))
 
     files_after = list(Path.cwd().iterdir())
 
@@ -55,10 +73,17 @@ def download_model(destination = None):
     shutil.move(folder_name_TEM_model.joinpath("model_seg_mouse_axon-myelin_tem"), str(tem_destination))
     shutil.move(folder_name_BF_model.joinpath("model_seg_rat_axon-myelin_bf"), str(bf_destination))
 
+    print("Models are stored in the directory: ", sem_destination.parent)
+
     # remove temporary folders
     shutil.rmtree(folder_name_SEM_model)
     shutil.rmtree(folder_name_TEM_model)
     shutil.rmtree(folder_name_BF_model)
 
 def main(argv=None):
-    download_model()
+    ap = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    # Setting the argument for specifying the directory where you wish to download the models
+    ap.add_argument("-p", "--path", required=False, help='Path where you wish to save your models', default = None)
+    args = vars(ap.parse_args(argv))
+    destination = args["path"]
+    download_model(destination)
--- test/test_download_models.py
+++ test/test_download_models.py
@@ -2,10 +2,11 @@
 
 from pathlib import Path
 import shutil
+import os
 
 import pytest
 
-from AxonDeepSeg.download_model import download_model
+from AxonDeepSeg.download_model import download_model, main
 
 
 class TestCore(object):
@@ -48,9 +49,15 @@ class TestCore(object):
         if tmpPath.exists():
             shutil.rmtree(tmpPath)
             pass
+        
+        #Delete the SEM and TEM model's if present in the test/models directory
+        if self.sem_model_path.parent == fullPath:
+            shutil.rmtree(self.sem_model_path)
+            shutil.rmtree(self.tem_model_path)
+            shutil.rmtree(self.bf_model_path)
 
     # --------------download_models tests-------------- #
-    @pytest.mark.unit
+    @pytest.mark.single
     def test_download_models_works(self):
         assert not self.sem_model_path.exists()
         assert not self.tem_model_path.exists()
@@ -62,9 +69,33 @@ class TestCore(object):
         assert self.tem_model_path.exists()
         assert self.bf_model_path.exists()
 
-    @pytest.mark.unit
+    @pytest.mark.single
     def test_redownload_models_multiple_times_works(self):
 
         download_model(self.tmpPath)
         download_model(self.tmpPath)
         
+    @pytest.mark.single
+    def test_download_models_working_directory(self):
+        self.sem_model_path = self.fullPath / "model_seg_rat_axon-myelin_sem"
+        self.tem_model_path = self.fullPath / "model_seg_mouse_axon-myelin_tem"
+        self.bf_model_path = self.fullPath / "model_seg_rat_axon-myelin_bf"
+        download_model(self.fullPath)
+
+        assert self.sem_model_path.exists()
+        assert self.tem_model_path.exists()
+        assert self.bf_model_path.exists()
+
+
+    # --------------download_models (cli) test-------------- #
+    @pytest.mark.single
+    def test_download_models_cli_runs_successfully_with_valid_destination_folder(self):
+        destination_path = self.fullPath    # set the destination path to test/models
+        self.sem_model_path = self.fullPath / "model_seg_rat_axon-myelin_sem"
+        self.tem_model_path = self.fullPath / "model_seg_mouse_axon-myelin_tem"
+        self.bf_model_path = self.fullPath / "model_seg_rat_axon-myelin_bf"
+
+        main(["-p", str(destination_path)])
+        assert self.sem_model_path.exists()
+        assert self.tem_model_path.exists()
+        assert self.bf_model_path.exists()
