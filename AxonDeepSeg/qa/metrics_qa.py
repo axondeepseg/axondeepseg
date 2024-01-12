a9792d19ce1d082fb1b39665a82031c0c6776f10
Mathieu Boudreau <emb6150@gmail.com>
2024-01-12 09:04:41 -0400

    Checkpoint

--- AxonDeepSeg/qa/metrics_qa.py
+++ AxonDeepSeg/qa/metrics_qa.py
@@ -7,6 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from pathlib import Path
+
 class MetricsQA:
     def __init__(self, morphometrics_file):
         """
@@ -14,15 +16,18 @@ class MetricsQA:
         :return: the pixel size value.
         """
 
-        self.file_name = morphometrics_file
+        self.file_name = Path(morphometrics_file)
 
-        self.df = pd.read_excel(morphometrics_file)
+        self.df = pd.read_csv(morphometrics_file)
+
+    def list_metrics(self):
+        print('\n'.join(list(self.df.columns.values[3:])))
 
     def plot(self, metric_name):
         x = self.df[metric_name].to_numpy()
 
         # bins='fd' uses the Freedman Diaconis Estimator to find the optimal number of bins
-        count, bins, ignored = plt.hist(x, 20, bins='fd', histtype='bar', ec='black')
+        count, bins, ignored = plt.hist(x,bins='fd', histtype='bar', ec='black')
 
         plt.show()
     
