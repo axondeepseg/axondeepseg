4f68ad416abb7f9c72cc3569aae2384e4179b7a9
Mathieu Boudreau, PhD <emb6150@gmail.com>
2024-08-02 15:50:49 -0300

    Update integrity_test.py

--- AxonDeepSeg/integrity_test.py
+++ AxonDeepSeg/integrity_test.py
@@ -12,9 +12,9 @@ import shutil
 # AxonDeepSeg imports
 #from AxonDeepSeg.testing.segmentation_scoring import pw_dice
 #from AxonDeepSeg.apply_model import axon_segmentation
-#import AxonDeepSeg.ads_utils as ads
-#import AxonDeepSeg.ads_utils
-#from config import axonmyelin_suffix
+import AxonDeepSeg.ads_utils as ads
+import AxonDeepSeg.ads_utils
+from config import axonmyelin_suffix
 
 def integrity_test():
 
