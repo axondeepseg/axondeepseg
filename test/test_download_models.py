56b087e344a0993327fc2cdce19ba0a8d961e0fa
Mathieu Boudreau <emb6150@gmail.com>
2024-11-19 22:02:02 -0400

    Add cli test

--- test/test_download_models.py
+++ test/test_download_models.py
@@ -81,6 +81,16 @@ class TestCore(object):
         assert (pytest_wrapped_e.type == SystemExit) and (pytest_wrapped_e.value.code == 0)
 
     @pytest.mark.integration
+    def test_main_cli_downloads_to_path(self):
+        cli_test_path = self.tmpPath / 'cli_test'
+        cli_test_model_path = cli_test_path / 'model_seg_generalist_light'
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            AxonDeepSeg.download_model.main(["-d", cli_test_path])
+
+        assert cli_test_model_path.exists()
+
+
+    @pytest.mark.integration
     def test_main_cli_fails_for_model_that_does_not_exist(self):
         model_name = "no_model"
         with pytest.raises(SystemExit) as pytest_wrapped_e:
