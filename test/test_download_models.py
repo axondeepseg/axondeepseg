a2d180f322d2a557af715a0f5218fa2b41dc6def
Mathieu Boudreau <emb6150@gmail.com>
2024-11-19 22:25:22 -0400

    Edit test

--- test/test_download_models.py
+++ test/test_download_models.py
@@ -80,12 +80,13 @@ class TestCore(object):
 
         assert (pytest_wrapped_e.type == SystemExit) and (pytest_wrapped_e.value.code == 0)
 
-    @pytest.mark.integration
+    @pytest.mark.single
     def test_main_cli_downloads_to_path(self):
         cli_test_path = self.tmpPath / 'cli_test'
         cli_test_model_path = cli_test_path / 'model_seg_generalist_light'
+        breakpoint()
         with pytest.raises(SystemExit) as pytest_wrapped_e:
-            AxonDeepSeg.download_model.main(["-d", cli_test_path])
+            AxonDeepSeg.download_model.main(["-d", str(cli_test_path)])
 
         assert cli_test_model_path.exists()
 
