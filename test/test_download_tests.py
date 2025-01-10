54c04130095b8728890d2ff23a505b5788429f85
Mathieu Boudreau <emb6150@gmail.com>
2025-01-10 13:43:06 -0400

    Add tests to ensure test precision files are the dtype and size expected - they can be easily changed by accident

--- test/test_download_tests.py
+++ test/test_download_tests.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import shutil
+import imageio
 
 import pytest
 
@@ -56,3 +57,44 @@ class TestCore(object):
     def test_redownload_test_files_multiple_times_works(self):
 
         download_tests(self.tmpPath)
+
+    @pytest.mark.single
+    def test_precision_images_expected_types(self):
+
+        precision_path = Path('test/__test_files__/__test_precision_files__')
+        rgb_path = Path('test/__test_files__/__test_color_files__')
+        rgba_path = Path('test/__test_files__/__test_coloralpha_files__')
+
+        filenames = (
+            'image_8bit.png',
+            'image_8bit.tif',
+            'image_16bit.png',
+            'image_16bit.tif',
+        )
+
+        expected_dtypes = (
+            'uint8',
+            'uint8',
+            'uint16',
+            'uint16',
+        )
+
+        for filename, expected_dtype  in zip(filenames, expected_dtypes):
+            _img = imageio.v3.imread(precision_path / filename)
+
+            assert str(_img.dtype) == expected_dtype
+            assert len(_img.shape) == 2
+
+            if (rgb_path / filename).exists():
+                _img = imageio.v3.imread(rgb_path / filename)
+
+                assert str(_img.dtype) == expected_dtype
+                assert len(_img.shape) == 3
+                assert _img.shape[-1] == 3
+
+            if (rgba_path / filename).exists():
+                _img = imageio.v3.imread(rgba_path / filename)
+
+                assert str(_img.dtype) == expected_dtype
+                assert len(_img.shape) == 3
+                assert _img.shape[-1] == 4
