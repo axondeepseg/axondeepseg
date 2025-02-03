d9cc40a045076ca680933bb1c81bd5b6b286927d
Mathieu Boudreau <emb6150@gmail.com>
2025-02-03 01:13:49 -0400

    Update tooltip

--- plugins/napari_ADS/_widget.py
+++ plugins/napari_ADS/_widget.py
@@ -195,7 +195,7 @@ class ADSplugin(QWidget):
         remove_axons_button.setStyleSheet(
             "QPushButton:checked{background-color:blue;}"
         )
-        remove_axons_button.setToolTip("Hold CONTROL/COMMAND and click on an axon to remove it in the axon and myelin masks.")
+        remove_axons_button.setToolTip("Hold CONTROL/COMMAND and click on an axon to remove it in the axon and myelin masks.\nHistology image must be active layer.")
         self.remove_axons_button = remove_axons_button
 
         save_segmentation_button = QPushButton("Save segmentation")
@@ -212,7 +212,7 @@ class ADSplugin(QWidget):
         show_axon_metrics_button.clicked.connect(
             self._on_show_axon_metrics
         )
-        show_axon_metrics_button.setToolTip("Hold ALT/OPTION and click on an axon to show key metrics.")
+        show_axon_metrics_button.setToolTip("Hold ALT/OPTION and click on an axon to show key metrics.\nHistology image must be active layer.")
 
         self.show_axon_metrics_button = show_axon_metrics_button
 
