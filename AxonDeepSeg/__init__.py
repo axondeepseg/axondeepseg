922dc84cd0ae0c757256879cf4cea99de7274622
Mathieu Boudreau <emb6150@gmail.com>
2025-02-07 13:21:16 -0400

    Remove pygit2 & commit logging - breaks when ADS not downloaded as a git repo

--- AxonDeepSeg/__init__.py
+++ AxonDeepSeg/__init__.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import pygit2
 
 __name__ = "AxonDeepSeg"
 
@@ -11,8 +10,3 @@ with open(version_file, 'r') as f:
 # make sure repo directory (two directories up) is on path (to access config.py)
 __repo_dir__ = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
 sys.path.append(__repo_dir__)
-
-repo = pygit2.Repository(__repo_dir__)
-__git_version__ = str(repo.head.target)
-__git_name__ = repo.head.shorthand
-__version_string__ = f"{__name__} v{__version__} ({__git_name__}: {__git_version__})"
