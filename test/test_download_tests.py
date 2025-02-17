f0d4c7799526ee519046774a52d9ee9fef6157fc
Mathieu Boudreau <emb6150@gmail.com>
2025-02-16 20:33:29 -0400

    Be more explicite when moving folders

--- test/test_download_tests.py
+++ test/test_download_tests.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from pathlib import Path
-import shutil
+import shutil, os
 import imageio
 
 import pytest
@@ -31,7 +31,7 @@ class TestCore(object):
             self.tmpPath.mkdir()
         
 
-        self.test_files_path = (
+        self.temp_test_files = (
             self.tmpPath /
             '__test_files__'
             )
@@ -61,26 +61,26 @@ class TestCore(object):
     # --------------download_model tests-------------- #
     @pytest.mark.integration
     def test_download_tests_runs_succesfully_with_destination(self):
-        assert not self.test_files_path.exists()
+        assert not self.temp_test_files.exists()
 
         download_tests(self.tmpPath)
 
-        assert self.test_files_path.exists()
+        assert self.temp_test_files.exists()
 
-    @pytest.mark.integration
+    @pytest.mark.single
     def test_main_cli_runs_succesfully_no_destination(self):
 
-        if self.test_files_path.exists():
-            shutil.rmtree(self.test_files_path)
-            shutil.mkdir(self.test_files_path)
-        dirtree = list((self.testPath / '__test_files__').iterdir())
+        if self.temp_test_files.exists():
+            shutil.rmtree(self.temp_test_files)
+            shutil.mkdir(self.temp_test_files)
+        dirtree = os.listdir((self.testPath / '__test_files__'))
+
         for file in dirtree:
             # Make temp dir
-            shutil.move(file, self.test_files_path)
+            shutil.move(self.testPath / '__test_files__' / file, self.temp_test_files / file)
 
         shutil.rmtree(self.testPath / '__test_files__')
         assert (self.testPath / '__test_files__').exists() == False
-
 
         AxonDeepSeg.download_tests.main([])
 
@@ -93,11 +93,11 @@ class TestCore(object):
         if not (self.testPath / '__test_files__').exists():
             (self.testPath / '__test_files__').mkdir()
 
-        dirtree = list((self.test_files_path).iterdir())
+        dirtree = os.listdir(self.temp_test_files)
         for file in dirtree:
-            shutil.move(file, self.testPath / '__test_files__')
+            shutil.move(self.temp_test_files / file, self.testPath / '__test_files__' / file)
 
-        shutil.rmtree(self.test_files_path)
+        shutil.rmtree(self.temp_test_files)
 
     @pytest.mark.unit
     def test_redownload_test_files_multiple_times_works(self):
