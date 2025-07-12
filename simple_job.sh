89223012ce6fdf1920940fbcd80a9a842ef35589
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2025-07-12 14:32:10 -0400

    requirements for sh job

---
+++ simple_job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --account=def-shahabkb
+#SBATCH --mem=60000
+#SBATCH -c 8
+#SBATCH --gpus=1
+#SBATCH --time=0-03:00           # time (DD-HH:MM)
+#SBATCH --mail-user=murielle.mardenli@gmail.com
+#SBATCH --mail-type=ALL
+
+#SBATCH --output=output.log
+#SBATCH --error=error.log
+
+module load StdEnv/2020
+
+module load python/3.11
+
+# Create virtual environment if non existent
+VENV_DIR=$HOME/envs/myenv
+if [ ! -d "$VENV_DIR" ]; then
+    python -m venv $VENV_DIR
+fi
+
+source $VENV_DIR/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run segmentation
+axondeepseg -i data_axondeepseg_sem/sub-rat1/micr/sub-rat1_sample-data1_SEM.png
